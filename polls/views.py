a544a371070749c52ac7c204483ba40bc75e7d18
grael@comcast.net <cjgtsm79!>
2015-10-13 14:30:05 -0600

    changed the details function

--- polls/views.py
+++ polls/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from .models import Question
 from django.template import RequestContext, loader
 from django.http import Http404
-
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 
@@ -33,12 +33,11 @@ def index(request):
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
+
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
+
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
@@ -46,6 +45,4 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
-
-
 
