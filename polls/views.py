b2ad1d9ea8260025320057a3f5db3e8a521f315a
grael@comcast.net <cjgtsm79!>
2015-10-13 14:16:06 -0600

    made changed to the detail function

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
 from django.template import RequestContext, loader
+from django.http import Http404
 
 
 # Create your views here.
@@ -17,7 +18,7 @@ def polls(request):
 
 """
 
-
+"""
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template('polls/index.html')
@@ -25,10 +26,19 @@ def index(request):
         'latest_question_list': latest_question_list,
     })
     return HttpResponse(template.render(context))
+"""
 
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
