1364e3039e7e517067ad1cab5f686bf8398a543a
grael@comcast.net <cjgtsm79!>
2015-10-13 14:01:39 -0600

    changed the urls.py file per tutorial

--- my_polls_site/urls.py
+++ my_polls_site/urls.py
@@ -2,11 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'my_polls_site.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^$', 'polls.views.index', name='index'),
-    url(r'^quiz/', 'polls.views.polls', name='polls'),
-    url(r'^account/', include('registration.backends.default.urls')),
+    url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
