eb0d37325678318f7fa3c4fc45e06229e81abca1
grael@comcast.net <cjgtsm79!>
2015-10-13 18:06:31 -0600

    added a url

--- my_polls_site/urls.py
+++ my_polls_site/urls.py
@@ -1,7 +1,19 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+"""
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+]
+"""
+
+# Include namespace in the url's
+
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 ]
