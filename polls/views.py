cbd96276eb1b3d9d0757ae4a57d15acfabdb1913
isaac <grael@comcast.net>
2018-01-26 10:40:19 -0800

    fixed the def vote function so that it sums the votes

--- polls/views.py
+++ polls/views.py
@@ -47,6 +47,7 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 """
 
+"""
 
 def vote(request, question_id):
     p = get_object_or_404(Question, pk=question_id)
@@ -78,7 +79,37 @@ def vote(request, question_id):
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
 
+"""
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_answer = question.answer_set.get(pk=request.POST['answer'])
+    except (KeyError, Answer.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select an answer.",
+        })
+    else:
+        selected_answer.votes += 1
+        selected_answer.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+
+
+
+
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
+
+
+
+
